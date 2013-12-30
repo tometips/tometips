ddf944b2869c41784b7c9611b2f937a22970e892
Josh Kelley <joshkel@gmail.com>
2013-12-29 22:12:02 -0500

    Add combat scaling so that code will work with 1.0.5 and above.
    DELETE_ON_ERROR

--- spoilers.lua
+++ spoilers.lua
@@ -1,6 +1,8 @@
 require 'engine.utils'
 require 'lib.json4lua.json.json'
 
+Actor = {}
+
 -- FIXME: Figure out where these should go and what they should do
 resolvers = {
     equip = function() end,
@@ -36,18 +38,18 @@ function newDamageType(t)
 end
 
 -- from engine.interface.ActorTalents
-talents_types_def = {}
+Actor.talents_types_def = {}
 function newTalentType(t)
     t.description = t.description or ""
     t.points = t.points or 1
     t.talents = {}
     -- Omit for cleaner JSON
     -- table.insert(self.talents_types_def, t)
-    talents_types_def[t.type] = t
+    Actor.talents_types_def[t.type] = t
 end
 
 -- from engine.interface.ActorTalents
-talents_def = {}
+Actor.talents_def = {}
 function newTalent(t)
     if type(t.type) == "string" then t.type = {t.type, 1} end
     if not t.type[2] then t.type[2] = 1 end
@@ -67,10 +69,11 @@ function newTalent(t)
     t.info = function(self, t) return info(self, t):gsub("\n\t+", "\n") end
 
     t.id = "T_"..t.short_name
-    talents_def[t.id] = t
+    Actor.talents_def[t.id] = t
+    Actor[t.id] = t.id
 
     -- Register in the type
-    table.insert(talents_types_def[t.type[1]].talents, t)
+    table.insert(Actor.talents_types_def[t.type[1]].talents, t)
 end
 
 load("/engine/colors.lua")
@@ -102,7 +105,20 @@ for i, v in ipairs(raw_resources) do
 end
 
 game = {}
-local player = {}
+local player = Actor
+
+player.getTalentRange = function(self, t)
+    if not t.range then return 1 end
+    if type(t.range) == "function" then return t.range(self, t) end
+    return t.range
+end
+
+--- Trigger a talent method
+player.callTalent = function(self, tid, name, ...)
+    local t = Actor.talents_def[tid]
+    name = name or "trigger"
+    if t[name] then return t[name](self, t, ...) end
+end
 
 -- Compute a diminishing returns value based on talent level that scales with a power
 -- t = talent def table or a numeric value
@@ -134,20 +150,93 @@ player.combatTalentScale = function(self, t, low, high, power, add, shift, raw)
     end
 end
 
+-- Compute a diminishing returns value based on a stat value that scales with a power
+-- stat == "str", "con",.... or a numeric value
+-- low = value to match when stat = 10
+-- high = value to match when stat = 100
+-- power = scaling factor (default 0.5) or "log" for log10
+-- add = amount to add the result (default 0)
+-- shift = amount to add to the stat value before computation (default 0)
+player.combatStatScale = function(self, stat, low, high, power, add, shift)
+	stat = type(stat) == "string" and self:getStat(stat,nil,true) or stat
+	power, add, shift = power or 0.5, add or 0, shift or 0
+	local x_low, x_high = 10, 100 -- Implied stat values to match
+	local x_low_adj, x_high_adj
+	if power == "log" then
+		x_low_adj, x_high_adj = math.log10(x_low+shift), math.log10(x_high+shift)
+		stat = math.max(1, stat)
+	else
+		x_low_adj, x_high_adj = (x_low+shift)^power, (x_high+shift)^power
+	end
+	local m = (high - low)/(x_high_adj - x_low_adj)
+	local b = low -m*x_low_adj
+	if power == "log" then -- always >= 0
+		return math.max(0, m * math.log10(stat + shift) + b + add)
+--		return math.max(0, m * math.log10(stat + shift) + b + add), m, b
+	else 
+		return math.max(0, m * (stat + shift)^power + b + add)
+--		return math.max(0, m * (stat + shift)^power + b + add), m, b
+	end
+end
+
+-- Compute a diminishing returns value based on talent level that cannot go beyond a limit
+-- t = talent def table or a numeric value
+-- limit = value approached as talent levels increase
+-- high = value at talent level 5
+-- low = value at talent level 1 (optional)
+-- raw if true specifies use of raw talent level
+--    returns (limit - add)*TL/(TL + halfpoint) + add == add when TL = 0 and limit when TL = infinity
+-- TL = talent level, halfpoint and add are internally computed to match the desired high/low values
+-- note that the progression low->high->limit must be monotone, consistently increasing or decreasing
+player.combatTalentLimit = function(self, t, limit, low, high, raw)
+    local x_low, x_high = 1,5 -- Implied talent levels for low and high values respectively
+    local tl = type(t) == "table" and (raw and self:getTalentLevelRaw(t) or self:getTalentLevel(t)) or t
+    if low then
+        local p = limit*(x_high-x_low)
+        local m = x_high*high - x_low*low
+        local halfpoint = (p-m)/(high - low)
+        local add = (limit*(x_high*low-x_low*high) + high*low*(x_low-x_high))/(p-m)
+        return (limit-add)*tl/(tl + halfpoint) + add
+--        return (limit-add)*tl/(tl + halfpoint) + add, halfpoint, add
+    else
+        local add = 0
+        local halfpoint = limit*x_high/(high-add)-x_high
+        return (limit-add)*tl/(tl + halfpoint) + add
+--        return (limit-add)*tl/(tl + halfpoint) + add, halfpoint, add
+    end
+end
+
+player.isTalentActive = function() return false end  -- TODO: Doesn't handle spiked auras
 player.hasEffect = function() return false end
 player.getSoul = function() return math.huge end
 player.knowTalent = function() return false end
+player.getInscriptionData = function()
+    return {
+        range = "varies"
+    }
+end
 
-function get_talent_level_val(val, actor, t)
+-- Overrides data/talents/psionic/psionic.lua.  TODO: Can we incorporate this at all?
+function getGemLevel()
+    return 0
+end
+
+function getByTalentLevel(actor, f)
+    local result = {}
+    for i = 1, 5 do
+        actor.getTalentLevel = function() return i end
+        actor.getTalentLevelRaw = function() return i end
+        result[#result+1] = tostring(f())
+        if result[#result] == result[#result-1] then result[#result] = nil end
+    end
+    actor.getTalentLevel = nil
+    actor.getTalentLevelRaw = nil
+    return #result > 0 and table.concat(result, ", ") or nil
+end
+
+function getvalByTalentLevel(val, actor, t)
     if type(val) == "function" then
-        local result = {}
-        for i = 1, 5 do
-            actor.getTalentLevel = function() return i end
-            result[#result+1] = tostring(val(actor, t))
-            if result[#result] == result[#result-1] then result[#result] = nil end
-        end
-        actor.getTalentLevel = nil
-        return table.concat(result, ", ")
+        return getByTalentLevel(actor, function() return val(actor, t) end)
     elseif type(val) == "table" then
         return val[rng.range(1, #val)]
     else
@@ -155,10 +244,22 @@ function get_talent_level_val(val, actor, t)
     end
 end
 
-for tid, t in pairs(talents_def) do
+for tid, t in pairs(Actor.talents_def) do
     t.mode = t.mode or "activated"
 
     if t.mode ~= "passive" then
+        if t.range == archery_range then
+            t.range = "archery"
+        else
+            local success, value = pcall(function() getByTalentLevel(player, function() return player:getTalentRange(t) end) end)
+            if not success then
+                io.stderr:write(string.format("%s: range: %s\n", tid, value))
+            else
+                t.range = value
+            end
+        end
+        if t.range == 1 then t.range = "melee/personal" end
+
         if t.no_energy and type(t.no_energy) == "boolean" and t.no_energy == true then
             t.use_speed = "instant"
         else
@@ -169,7 +270,7 @@ for tid, t in pairs(talents_def) do
     for i, v in ipairs(raw_resources) do
         cost = {}
         if t[v] then
-            cost[#cost+1] = string.format("%s %s", get_talent_level_val(t[v], player, t), resources[v])
+            cost[#cost+1] = string.format("%s %s", getvalByTalentLevel(t[v], player, t), resources[v])
         end
         if #cost > 0 then t.cost = table.concat(cost, ", ") end
     end
@@ -190,8 +291,8 @@ if true then
         colors = colors,
         -- FIXME: Strip death_message
         DamageType = DamageType,
-        talents_types_def = talents_types_def,
-        talents_def = talents_def
+        talents_types_def = Actor.talents_types_def,
+        talents_def = Actor.talents_def
     }))
 end
 
